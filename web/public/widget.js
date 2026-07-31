1b20fc088a3ab9e6743acfbfbfc93c28e65ec9e0
fer-gif <ferminhernando00@gmail.com>
2026-07-31 12:02:37 -0300

    fix: update widget promo link to point to https://dataene.com.ar/futbol-neco/

--- web/public/widget.js
+++ web/public/widget.js
@@ -208,7 +208,7 @@
       }
       const nombreMedio = this.getAttribute("client-name") || "Diario DataNE";
       const clientId = this.getAttribute("client-id") || "";
-      const prodeUrl = `${baseUrl}/prode${clientId ? `?client-id=${clientId}` : ""}`;
+      const prodeUrl = this.getAttribute("prode-url") || "https://dataene.com.ar/futbol-neco/";
 
       const styleTemplate = `
         <style>
@@ -569,7 +569,7 @@
           ${this.estilo.mostrar_prode ? `
             <div class="prode-promo-bar">
               <span style="color:#a1a1aa; font-size:11px; font-weight:700;">¿Quién gana esta fecha?</span>
-              <a href="${prodeUrl}" target="_blank" class="prode-promo-link">
+              <a href="${prodeUrl}" target="_top" class="prode-promo-link">
                 ¡Jugá al Prode de tu equipo y ganá! ➔
               </a>
             </div>
